b9ba982a305e8e5423233a2623e367dce9b21b07
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2023-10-30 21:29:57 +0200

    added hash of block

--- goblockchain.go
+++ goblockchain.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"log"
 	"time"
 )
 
 type Block struct {
 	nonce        int
-	previousHash string
+	previousHash [32]byte
 	timestamp    int64
 	transaction  []string
 }
 
-func NewBlock(nonce int, previousHash string) *Block {
+func NewBlock(nonce int, previousHash [32]byte) *Block {
 	b := new(Block)
 	b.timestamp = time.Now().UnixNano()
 	b.nonce = nonce
@@ -21,23 +23,47 @@ func NewBlock(nonce int, previousHash string) *Block {
 	return b
 }
 
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	return sha256.Sum256([]byte(m))
+}
+
+func (b *Block) MarshalJson() ([]byte, error) {
+	return json.Marshal(struct {
+		Nonce        int      `json:"nonce"`
+		PreviousHash [32]byte `json:"previous_hash"`
+		Timestamp    int64    `json:"time_stamp"`
+		Transaction  []string `json:"transaction"`
+	}{
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Timestamp:    b.timestamp,
+		Transaction:  b.transaction,
+	})
+}
+
 type Blockchain struct {
 	transactionpool []string
 	chain           []*Block
 }
 
 func NewBlockchain() *Blockchain {
+	b := &Block{}
 	bc := new(Blockchain)
-	bc.CreateBlock(0, "Init Hash")
+	bc.CreateBlock(0, b.Hash())
 
 	return bc
 }
 
-func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b)
 
 	return b
+}
+
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
 }
 
 func init() {
